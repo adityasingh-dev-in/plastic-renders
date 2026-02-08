82e84199155c4ce4d55584ed09a4c613b939020a
Aditya Singh <singhaditya8382@gmail.com>
2026-02-08 16:05:17 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Welcome to MERN App (TypeScript)</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
